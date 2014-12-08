0ab288f6996f3b3de916a35fd4993df4e46db9be
Alexey Huba <sadchi@gmail.com>
2014-12-08 13:46:14 +0300

    ref

--- project.clj
+++ project.clj
@@ -4,21 +4,28 @@
                  [reagent "0.4.3"]]
 
   :plugins      [[lein-cljsbuild "1.0.3"]
-                 ]
+                 [lein-haml-sass "0.2.7-SNAPSHOT"]]
 
 
-  :cljsbuild    {:builds 
-                 {:reporter 
-                  {:source-paths ["src"]
-                   :compiler 
-                   {:preamble ["reagent/react.js"]
-                    :output-to "target/reporter.js"
-                    :output-dir "target/out"
-                    :optimizations :simple 
-                    :pretty-print true
-                    :source-map "target/reporter.js.map"
-                    }
-                   }
-                  }
+  :sass {
+         :src "sass"
+         :output-directory "css"
+         :delete-output-dir true
+         }
+
+  :cljsbuild    {
+                 :builds {
+                          :reporter {
+                                     :source-paths ["src"]
+                                     :compiler {
+                                                :preamble ["reagent/react.js"]
+                                                :output-to "target/reporter.js"
+                                                :output-dir "target/out"
+                                                :optimizations :simple 
+                                                :pretty-print true
+                                                :source-map "target/reporter.js.map"
+                                                }
+                                     }
+                          }
                  }
   )
