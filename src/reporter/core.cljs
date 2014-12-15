b8e4df379286ba7cda9c399b8bbe76624f0138d6
Sadchi <sadchi@gmail.com>
2014-12-16 00:10:48 +0300

    ref

--- src/reporter/core.cljs
+++ src/reporter/core.cljs
@@ -3,12 +3,22 @@
 
 (def data (js->clj js/data :keywordize-keys true))
 
-(def sec-tree (atom (sorted-map)))
-(def sec-visibility (atom {} ))
+(def tree (atom (sorted-map)))
+(def vis-map  (atom {} ))
 
 (defn add-sec [coll path]
-  (swap! coll assoc-in path {:state :closed}))
+  (->> {:state :closed}
+       (get-in @coll path)
+       (#(assoc % :state :closed))
+       (swap! coll assoc-in path )))
 
+(defn add-sec-recur [coll path]
+  (loop [p path]
+    (if (empty? p)
+      nil
+      (do
+       (add-sec coll p)
+       (recur (pop p))))))
 
 (defn update-path [coll path func]
   (->> {:count 0 :visibility :visible}
@@ -31,14 +41,19 @@
        (recur (pop p))))))
 
 (defn inc-count-recur [coll path]
-  update-count coll path inc)
+  (update-count coll path inc)
+  )
 
 (defn dec-count-recur [coll path]
-  update-count coll path dec)
+  (update-count coll path dec)
+  )
 
 (defn initial-process-data [d tree vis-table]
   (doseq [item data]
-     (.log js/console (clj->js item)))
+    (let [path (:path item)]
+      (inc-count-recur vis-table path)
+      (add-sec-recur tree path)
+      ))
   )
 
 (defn header []
@@ -51,7 +66,7 @@
   [:div.container "TEST"])
 
 (defn main []
-  (initial-process-data data nil nil)   
+  (initial-process-data data tree vis-map                        )   
   [:div [header]
    [body]
    [footer]])
