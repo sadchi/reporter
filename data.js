5af9ce06f6d1e46404c83acf3bbbc1fddd5528b2
Alexey Huba <sadchi@gmail.com>
2014-12-15 20:01:16 +0300

    ref

--- data.js
+++ data.js
@@ -1,4 +1,24 @@
 var data=[
-{"status": "SUCCESS", "fails": [{"type": "AssertError.ExtractionError.ExcessiveExtraction", "msg": "Tooth with id #13 was extracted in final position, but it was expected to retain"}], "filename": "d:\\Temp\\1_1_0_3362533USTreat.adf", "path": ["A", "B", "C", "D"], "test": "T1", "meta": [{"type": "text", "data": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh enim, aliquam tristique arcu sed, vestibulum rhoncus libero. Cras lacinia a arcu ut sagittis. Aliquam erat volutpat. Cras laoreet et diam vitae consectetur. Nam sed commodo libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque vestibulum rutrum lacus pharetra scelerisque. Nullam faucibus cursus tempor. Sed dapibus ex non lectus aliquet venenatis. Suspendisse vitae purus tincidunt, varius urna eget, mollis libero. Donec tristique urna sit amet lacinia viverra."}], "exceptions": []},
-{"status": "FAIL", "fails": [{"type": "AssertError.ExtractionError.ExcessiveExtraction", "msg": "Tooth with id #13 was extracted in final position, but it was expected to retain"}], "filename": "d:\\Temp\\1_1_0_3362533USTreat.adf", "path": ["F", "B"], "test": "T2", "meta": [{"type": "text", "data": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh enim, aliquam tristique arcu sed, vestibulum rhoncus libero. Cras lacinia a arcu ut sagittis. Aliquam erat volutpat. Cras laoreet et diam vitae consectetur. Nam sed commodo libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque vestibulum rutrum lacus pharetra scelerisque. Nullam faucibus cursus tempor. Sed dapibus ex non lectus aliquet venenatis. Suspendisse vitae purus tincidunt, varius urna eget, mollis libero. Donec tristique urna sit amet lacinia viverra."}], "exceptions": []}
+    {
+        "path" : [ "A", "A1", "A11"]
+    },
+    {
+        "path" : [ "A", "A1", "A12"]
+    },
+    {
+        "path" : [ "A", "A2"]
+    },
+    {
+        "path" : [ "B", "B1"]
+    },
+    {
+        "path" : [ "C", "C1", "C11"]
+    },
+    {
+        "path" : [ "C", "C2", "C21"]
+    }
+
+
+// {"status": "SUCCESS", "fails": [{"type": "AssertError.ExtractionError.ExcessiveExtraction", "msg": "Tooth with id #13 was extracted in final position, but it was expected to retain"}], "filename": "d:\\Temp\\1_1_0_3362533USTreat.adf", "path": ["A", "B", "C", "D"], "test": "T1", "meta": [{"type": "text", "data": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh enim, aliquam tristique arcu sed, vestibulum rhoncus libero. Cras lacinia a arcu ut sagittis. Aliquam erat volutpat. Cras laoreet et diam vitae consectetur. Nam sed commodo libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque vestibulum rutrum lacus pharetra scelerisque. Nullam faucibus cursus tempor. Sed dapibus ex non lectus aliquet venenatis. Suspendisse vitae purus tincidunt, varius urna eget, mollis libero. Donec tristique urna sit amet lacinia viverra."}], "exceptions": []},
+// {"status": "FAIL", "fails": [{"type": "AssertError.ExtractionError.ExcessiveExtraction", "msg": "Tooth with id #13 was extracted in final position, but it was expected to retain"}], "filename": "d:\\Temp\\1_1_0_3362533USTreat.adf", "path": ["F", "B"], "test": "T2", "meta": [{"type": "text", "data": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh enim, aliquam tristique arcu sed, vestibulum rhoncus libero. Cras lacinia a arcu ut sagittis. Aliquam erat volutpat. Cras laoreet et diam vitae consectetur. Nam sed commodo libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque vestibulum rutrum lacus pharetra scelerisque. Nullam faucibus cursus tempor. Sed dapibus ex non lectus aliquet venenatis. Suspendisse vitae purus tincidunt, varius urna eget, mollis libero. Donec tristique urna sit amet lacinia viverra."}], "exceptions": []}
 ]
--- src/reporter/core.cljs
+++ src/reporter/core.cljs
@@ -1,8 +1,45 @@
 (ns reporter.core
   (:require [reagent.core :as r :refer [atom]]))
 
-(def counter (atom 0))
 (def data (js->clj js/data :keywordize-keys true))
+
+(def sec-tree (atom (sorted-map)))
+(def sec-visibility (atom {} ))
+
+(defn add-sec [coll path]
+  (swap! coll assoc-in path {:state :closed}))
+
+
+(defn update-path [coll path func]
+  (->> {:count 0 :visibility :visible}
+       (get @coll path)
+       ( #(update-in % [:count] func))
+       (swap! coll assoc path)))
+
+(defn add-path [coll path]
+  (update-path coll path inc))
+
+(defn rem-path [coll path]
+  (update-path coll path dec))
+
+(defn update-count [coll path func]
+ (loop [p path]
+    (if (empty? p)
+      nil
+      (do
+       (update-path coll p func)
+       (recur (pop p))))))
+
+(defn inc-count-recur [coll path]
+  update-count coll path inc)
+
+(defn dec-count-recur [coll path]
+  update-count coll path dec)
+
+(defn initial-process-data [d tree vis-table]
+  (doseq [item data]
+     (.log js/console (clj->js item)))
+  )
 
 (defn header []
   [:div.header "FAILED"])
@@ -14,8 +51,7 @@
   [:div.container "TEST"])
 
 (defn main []
-  (let [ [first & rest] data]
-    (.log js/console (:status first)))   
+  (initial-process-data data nil nil)   
   [:div [header]
    [body]
    [footer]])
