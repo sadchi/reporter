1c1e94a5d8d84e31fe65ee556b5d473ed8dc90e2
Alexey Huba <sadchi@gmail.com>
2014-12-17 20:22:00 +0300

    ref

--- data.js
+++ data.js
@@ -2,6 +2,14 @@ var data=[
     {
         "status" : "SUCCESS",
         "path" : [ "A", "A1", "A11", "T1"],
+        "fails" : [
+                {"type" : "A.E.E",
+                "msg" : "AAAAA Error Assert Error"},
+                {"type" : "B.E.E",
+                "msg" : "BBBBB Error Assert Error"},
+                {"type" : "C.E.E",
+                "msg" : "CCCCC Error Assert Error"}
+            ],
         "meta" : [
             {"type" : "table",
             "name" : "The first test table",
@@ -10,7 +18,9 @@ var data=[
                 [1, 2, 3, 4, 5],
                 [0, 1, 0, 0, 0],
                 [0, 0, 1, 0, 0],
-                [0, 0, 0, 1, 5]]}
+                [0, 0, 0, 1, 5]]},
+            {"type" : "text",
+            "data" : "Sample text."}
         ]
     },
     {
@@ -34,7 +44,4 @@ var data=[
         "path" : [ "C", "C2", "C21"]
     }
 
-
-// {"status": "SUCCESS", "fails": [{"type": "AssertError.ExtractionError.ExcessiveExtraction", "msg": "Tooth with id #13 was extracted in final position, but it was expected to retain"}], "filename": "d:\\Temp\\1_1_0_3362533USTreat.adf", "path": ["A", "B", "C", "D"], "test": "T1", "meta": [{"type": "text", "data": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh enim, aliquam tristique arcu sed, vestibulum rhoncus libero. Cras lacinia a arcu ut sagittis. Aliquam erat volutpat. Cras laoreet et diam vitae consectetur. Nam sed commodo libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque vestibulum rutrum lacus pharetra scelerisque. Nullam faucibus cursus tempor. Sed dapibus ex non lectus aliquet venenatis. Suspendisse vitae purus tincidunt, varius urna eget, mollis libero. Donec tristique urna sit amet lacinia viverra."}], "exceptions": []},
-// {"status": "FAIL", "fails": [{"type": "AssertError.ExtractionError.ExcessiveExtraction", "msg": "Tooth with id #13 was extracted in final position, but it was expected to retain"}], "filename": "d:\\Temp\\1_1_0_3362533USTreat.adf", "path": ["F", "B"], "test": "T2", "meta": [{"type": "text", "data": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh enim, aliquam tristique arcu sed, vestibulum rhoncus libero. Cras lacinia a arcu ut sagittis. Aliquam erat volutpat. Cras laoreet et diam vitae consectetur. Nam sed commodo libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque vestibulum rutrum lacus pharetra scelerisque. Nullam faucibus cursus tempor. Sed dapibus ex non lectus aliquet venenatis. Suspendisse vitae purus tincidunt, varius urna eget, mollis libero. Donec tristique urna sit amet lacinia viverra."}], "exceptions": []}
 ]
