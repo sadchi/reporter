14f28458e123ac3a4071bab8d873c206e9f04d03
Sadchi <sadchi@gmail.com>
2014-12-21 19:28:21 +0300

    ref

--- src/reporter/core.cljs
+++ src/reporter/core.cljs
@@ -5,19 +5,33 @@
 
 (def inner-test-results (js->clj js/data :keywordize-keys true))
 (def report-structure (atom (sorted-map)))
+(def overview-section (atom {}))
 (def extra-props  (atom {} ))
 (def zero-prop {:visibility :visible
-               :status 0
+               :status "UNDEFINED"
                :count 0
                :fail-count 0})
 
 (def bottom-level 4)
-(def status-rank {"UNDEFINED" 0
+(def highest-level 1)
+
+
+(def status-rank-mapping {"UNDEFINED" 0
                     "SUCCESS" 1
                     "FAIL" 2})
 
+(def rank-status-mapping {0 "UNDEFINED"
+                    1 "SUCCESS"
+                    2 "FAIL"})
+
+(defn status->rank [status]
+  (get status-rank-mapping status))
+
+(defn rank->status [rank]
+  (get rank-status-mapping rank))
+
 (defn add-sec! [coll path]
-  (->> {:state :opened}
+  (->> {:state :opened }
        (get-in @coll path)
        (#(assoc % :state :opened))
        (swap! coll assoc-in path )))
@@ -32,10 +46,11 @@
        (recur (pop p))))))
 
 
-(defn change-status [new-status-s old-status]
-  (->> new-status-s
-       (get status-rank)
-       (max old-status)))
+(defn change-status [new-status-s old-status-s]
+  (let [old-rank (status->rank old-status-s)
+        new-rank (status->rank new-status-s)]
+    (-> (max old-rank new-rank)
+        (rank->status))))
 
 (defn update-single! [acoll path attrib update-f]
   (-> @acoll
@@ -59,15 +74,13 @@
         (update-up-to-root! props path :fail-count inc))
       (add-sec-recur! tree path))))
 
-
-
 (defn flip-state [state]
   (cond
    (= state :opened) :closed
    (= state :closed) :opened))
 
-(defn flip-func! [path]
-  (swap! report-structure update-in (conj path :state) flip-state))
+(defn flip-func! [acoll path]
+  (swap! acoll update-in (conj path :state) flip-state))
 
 
 (defn gen-key []
@@ -80,12 +93,12 @@
                )]
     [:span {:class class}]))
 
-(defn heading [level s]
+(defn heading [level s extra-classes]
   (let [class (cond
                (< 0 level 6) (str "heading--h" level)
                :else "heading--h5"
                )]
-    [:div {:class (join " " ["heading" class "section__head--grow"])} s]))
+    [:div {:class (join " " ["heading" class "section__head--grow" extra-classes])} s]))
 
 
 
@@ -102,11 +115,20 @@
        (boolean)))
 
 
-(defn section-head [level state path]
+(defn heading-fails-stat [props path]
+  (let [prop (get props path)
+        fails (:fail-count prop)
+        total (:count prop)] 
+    [:span.heading.heading--h5.heading--no-padding
+     [:span.error fails]
+     [:span (str "/" total)]]))
+
+(defn section-head [{:keys [level state status path acoll extra]}]
   [:div.section__head 
-   {:on-click #(flip-func! path)}
+   {:on-click #(flip-func! acoll path)}
    [group-sign state]
-   [heading level (peek path)]])
+   [heading level (peek path) (when (= status "FAIL") "error")]
+   extra])
 
 
 (defn meta-text [meta-item]
@@ -169,31 +191,67 @@
      [meta-data-render meta-data]
      [fail-table fails]]))
 
-(defn test-result [path]
-  (let [state (get-in @report-structure (conj path :state))
+(defn test-result-special-mark [status]
+  [:span.test-mark-sign (when (= status "FAIL") {:class "error"})])
+
+(defn test-result [acoll path]
+  (let [state (get-in @acoll (conj path :state))
         test-info (get-test path)
         status (:status test-info)] 
-    [:div [section-head bottom-level state path]
+    [:div [section-head {:level bottom-level
+                         :state state
+                         :status status
+                         :path path
+                         :acoll acoll
+                         :extra (test-result-special-mark status)}]
      (when (= state :opened) [test-result-content test-info])]))
 
-
-(defn render-tree [tree level path]
+(defn render-tree [tree atree props level path]
   (for [k (keys tree)
         :when (not= k :state)]
     (let [v (get tree k)
           state (:state v)
-          p (conj path k)]
+          p (conj path k)
+          status (-> props (get p) (get :status))]
       [:div.section {:key (gen-key)} 
        (if (is-test? p)
-         ^{:key (gen-key)}[test-result p]
-         (list ^{:key (gen-key)}[section-head level state p]
+         ^{:key (gen-key)}[test-result atree p]
+         (list ^{:key (gen-key)}[section-head {:level level
+                                               :state state
+                                               :status status
+                                               :path p
+                                               :acoll atree
+                                               :extra (heading-fails-stat props p)}]
                (if (= state :opened)
-                 (render-tree v (inc level) p))))])))
+                 (render-tree v atree props (inc level) p))))])))
 
+
+
+
+(defn overall-stat-content []
+  [:div.inner-content
+   [:table.simple-table.simple-table--no-borders
+    [:tr
+     [:td.simple-table__td.simple-table--no-borders.simple-table--10.simple-table--right
+      "Total tests:"] 
+     [:td.simple-table__td.simple-table--no-borders
+      "24"]]]])
+
+(defn overall-stat [acoll path]
+  (let [state (get-in @acoll (conj path :state))]
+    [:div.section
+     [section-head {:level highest-level
+                    :state state
+                    :status "UNDEFINED"
+                    :path path
+                    :acoll acoll}]
+     (when (= state :opened) [overall-stat-content])]))
 
 
 (defn report-tree []
-  [:div.container (render-tree @report-structure 1 [])])
+  [:div.container
+   [overall-stat overview-section ["Overview"]]
+   (render-tree @report-structure report-structure @extra-props 1 [])])
 
 
 (defn header []
@@ -204,7 +262,10 @@
   [report-tree])
 
 
+
+
 (defn ^:export run []
   (initial-process-data! inner-test-results report-structure extra-props)
+  (add-sec! overview-section ["Overview"])
   (r/render-component [header] (.getElementById js/document "header"))
   (r/render-component [main] (.getElementById js/document "main")))
